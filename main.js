bbe2f476c0b57f7fde1ec9a4a3e3266e9bccbc0b
ebenahmed0 <110251165+ebenahmed0@users.noreply.github.com>
2022-08-08 18:02:23 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+<SCRIPT LANGUAGE="JavaScript">user = 'eben.ahmed';site = 'protonmail';document.write('<a href=\"mailto:' + user + '@' + site + '\">');document.write(user + '@' + site + '</a>');</SCRIPT>
