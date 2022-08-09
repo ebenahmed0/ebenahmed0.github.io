b3c1ff4bfdd0bb70c23a1ef8763ef069cef41f7a
ebenahmed0 <110251165+ebenahmed0@users.noreply.github.com>
2022-08-09 20:37:40 +0100

    Delete main.js

--- main.js
+++
@@ -1,1 +0,0 @@
-<SCRIPT LANGUAGE="JavaScript">user = 'eben.ahmed';site = 'protonmail';document.write('<a href=\"mailto:' + user + '@' + site + '\">');document.write(user + '@' + site + '</a>');</SCRIPT>
